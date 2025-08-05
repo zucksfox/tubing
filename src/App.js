58b263390d7b25edecc728e5121db5722b307c09
fahrurmon <fahrurans@gmail.com>
2025-08-05 18:27:06 +0700

    push

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import PricelistSection from './components/PricelistSection';
 import GallerySection from './components/GallerySection';
-import VideoProfileSection from './components/VideoProfileSection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
 import FloatingWhatsApp from './components/FloatingWhatsApp';
@@ -22,9 +21,6 @@ function App() {
       
       {/* Gallery Section */}
       <GallerySection />
-      
-      {/* Video Profile Section */}
-      <VideoProfileSection />
       
       {/* Contact Section */}
       <ContactSection />
